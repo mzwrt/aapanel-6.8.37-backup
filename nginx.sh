a414f32a5cfc0fe203c1a2c56797e39ade11a62d
mzal <58111339+mzwrt@users.noreply.github.com>
2024-06-29 23:25:24 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -3,6 +3,13 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+######### 检测是否是root用户 ###################
+if [ "$(id -u)" -ne 0 ]; then
+  echo "此脚本需要以root用户运行。"
+  exit 1
+fi
+echo "你是以root用户运行此脚本。脚本继续运行"
+
 public_file=/www/server/panel/install/public.sh
 . $public_file
 publicFileMd5=$(md5sum ${public_file} 2>/dev/null|awk '{print $1}')
